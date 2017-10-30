f5d7bd5edae32cf3500ce1314f678e26af2a9765
Drew Grubb <dgrubb@visualstatement.com>
2017-10-30 15:50:08 -0700

    Initial commit of a demo console app to showcase basic git functionality and feature branching processes

---
+++ GitDemoConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitDemoConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World: master branch");
+
+            //wait for user input so we can reat the screen
+            Console.WriteLine("\r\n\r\n\r\nPress 'Enter' to close");
+            Console.ReadLine();
+        }
+    }
+}
